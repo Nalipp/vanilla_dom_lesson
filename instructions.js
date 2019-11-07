39a24f34eeadd5c53eaac9acf332de12611c600d
Nalipp <nateplipp@gmail.com>
2019-11-07 11:31:26 -0800

    lesson 2

--- instructions.js
+++ instructions.js
@@ -1,21 +1,45 @@
-// The easiest way to select elements is by their id using the getElementById function on the document object (document.getElementById). This returns a SINGLE element (because ids must be unique!).
+// Modifying properties and attributes on elements in the DOM
+// We can change the text of an element through the innerHTML property.
 
-var container = document.getElementById("container");
+var firstDiv = document.getElementsByTagName("div")[0];
 
-// We can also use a function called querySelector, which selects a SINGLE element using CSS selectors. If multiple elements match the query you pass in to querySelector, the function will simply return the first matching element that it finds.
+firstDiv.innerHTML = "Just changed!";
+// This can also be done using the innerText property.
 
-var container = document.querySelector("#container");
+var secondDiv = document.getElementsByTagName("div")[1];
 
-// Notice that when we select by id using querySelector, we pass in the string #container, not container. Remember: querySelector always expects a CSS selector. In contrast, when we use getElementById, we just pass in the string container (no hashtag)! Since getElementById expects to find an element by id, in this case the hashtag isn't necessary.
+secondDiv.innerText = "Just changed Again!";
+// What's the difference between innerText and innerHTML? You can find it here.
 
-// To select multiple elements, we can use getElementsByTagName or getElementsByClassName, or we can use querySelectorAll and pass in a CSS selector. These will return what appear to be arrays (they are not exactly arrays, but for right now, that is not a problem).
+// We can also directly manipulate the CSS properties for elements (through inline styling) with the style property.
 
-var divs = document.getElementsByTagName("div");
-var divs = document.querySelectorAll("div");
-// Here is another example using getElementsByClassName and the same thing with querySelectorAll.
+var firstDiv = document.getElementsByTagName("div")[0];
 
-var divsWithClassOfHello = document.getElementsByClassName("hello");
-var divsWithClassOfHello = document.querySelectorAll(".hello");
-// As you can see, when you pass in a class name using getElementsByClassName, you don't need to start the string with a dot. The function expects to receive a class name - it's in the name of the function! On the other hand, querySelectorAll takes in any valid CSS query, which is why you need to pass in .hello if you want it to find all elements with a class of "hello."
+firstDiv.style.color = "red";
+firstDiv.style.backgroundColor = "teal";
+// Notice that if you're accessing CSS properties using dot notation, you need to camelCase those property names, since firstDiv.style.background-color is invalid JavaScript. 
 
-// When you're ready, move on to DOM Manipulation
+// (Bonus question: what do you think will happen if you try typing this in the console?) // Another option is to avoid the camel case by using brackets
+
+firstDiv.style["background-color"] = "purple"; // this works too
+// If we want to access/modify attributes on elements, we can do that with getAttribute and setAttribute:
+
+var body = document.getElementById("container");
+
+body.getAttribute("id"); // "container"
+body.setAttribute("id", "new_container");
+body.getAttribute("id"); // "new_container"
+// We can also add and remove classes to elements using classList
+
+var secondDiv = document.getElementsByTagName("div")[1];
+
+secondDiv.classList; // ["hello"]
+secondDiv.classList.add("another_class");
+secondDiv.classList; // ["hello", "another_class"]
+secondDiv.classList.remove("hello");
+secondDiv.classList; // [another_class"]
+// Notice here that we use methods called add and remove, not the push and pop methods we've seen when dealing with arrays. This is because the classList isn't actually an array and doesn't have push or pop methods on it.
+
+// Nodes vs Elements
+// When you read documentation about the DOM, you will see the terms node and element. In JavaScript these two are different and you can read more about the difference here. In short, there are many different types of nodes, but the ones we will most often be working with are ELEMENT_NODE and TEXT_NODE (you can see them all here). Element nodes are HTML elements (div, span etc.). Text nodes are the actual text of an element node.
+
